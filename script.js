103da0714c0543ded813f0b4f9540137d87bbc6b
Sharique-code93 <shariquesk43@gmail.com>
2026-05-03 11:47:49 +0530

    Refactor scroll animation and add navbar shadow effect

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+// SCROLL ANIMATION
 const elements = document.querySelectorAll('.fade-up, .fade-left, .fade-right');
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        if (entry.isIntersecting) {
+        if(entry.isIntersecting){
             entry.target.classList.add('show');
         }
     });
-}, { threshold: 0.2 });
+}, {threshold: 0.2});
 
 elements.forEach(el => observer.observe(el));
+
+
+// NAVBAR SHADOW ON SCROLL
+window.addEventListener("scroll", () => {
+    const nav = document.getElementById("navbar");
+
+    if (window.scrollY > 50) {
+        nav.style.background = "rgba(0,0,0,0.7)";
+    } else {
+        nav.style.background = "rgba(0,0,0,0.3)";
+    }
+});
